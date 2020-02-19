4ae3407a2799b2525068ccd6fb57aef104a6fd96
Danilkashtan <36625284+Danilkashtan@users.noreply.github.com>
2020-02-19 13:42:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//import 'core-js/stable'
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App';
+
+// Importing the Bootstrap CSS
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <h1>Привет, мир!</h1>,
+  document.getElementById('root')
+);
